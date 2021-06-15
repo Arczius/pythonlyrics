89a8f1e01e068a29bba0cc5141247c95e1e747db
Damian Vaartmans <99061149@mydavinci.nl>
2021-06-15 11:42:58 +0200

    Create lyrics.py

---
+++ lyrics.py
@@ -0,0 +1,17 @@
+import lyricsgenius
+genius = lyricsgenius.Genius("your token")
+
+row = "------------"
+
+print("what is the name of the artist?")
+artistName = input("-")
+artist = genius.search_artist(artistName, max_songs=3, sort="popularity")
+print(row)
+
+print("what is the name of the song?")
+songName = input("-")
+print(row)
+
+song = genius.search_song(songName, artist.name)
+
+print(song.lyrics)
